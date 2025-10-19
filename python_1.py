a0a25537a5cf2bea421790b2cc66c20701a4e71d
Ryas1k <ryaskovi@mail.ru>
2025-10-19 21:17:23 +0700

    Update python_1.py

--- python_1.py
+++ python_1.py
@@ -2,4 +2,5 @@ numbers= [str(_) for _ in range(1,int(input())+1)]
 slovar = {}
 for n in numbers:
     slovar[sum(map(int,list(n)))] = slovar.get(sum(map(int,list(n))),0)+1
-print(slovar)
+
+print(max(slovar.values()))
